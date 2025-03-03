ebb6ef0208b8e9f675d19a8e85db677e14de963b
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-03-03 19:07:27 +0800

    nestjs 增加swagger文档

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(8000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const config = new DocumentBuilder()
+    .setTitle('接口文档')
+    .setDescription('描述')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(8000, () => {
+    console.log('127.0.0.1:8000');
+  });
 }
+
 bootstrap();
